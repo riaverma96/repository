284b65862c0d32f844c853cf9f02b6870622c001
Ria Verma <rverma35@gatech.edu>
2015-08-31 17:32:53 -0400

    right shift by 2

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -1,4 +1,5 @@
 package edu.gatech.oad.antlab.person;
+package java.util.Math;
 
 /**
  *  A simple class for person 1
@@ -10,7 +11,7 @@ package edu.gatech.oad.antlab.person;
  */
 public class Person1 {
   /** Holds the persons real name */
-  private String name;
+  private String name = "Ria Verma";
   	/**
 	 * The constructor, takes in the persons
 	 * name
@@ -31,7 +32,15 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	    String temp = input;
+		for (int i = 0; i < name.length; i++) {
+            if (i >= 2) {
+                name.charAt(i) = temp.charAt((i-2));
+            } else {
+                name.charAt(i) = temp.charAt((i+1));
+            }
+		}
+		return name;
 	}
 	
 	/**
