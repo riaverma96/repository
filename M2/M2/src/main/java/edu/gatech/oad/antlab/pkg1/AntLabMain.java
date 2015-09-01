4ea9b0c20ecf95950456b041dadb0398cea22ed0
dbanerjee30 <dbrbbff7814@gmail.com>
2015-09-01 14:55:35 -0400

    Wrote method to reverse a String

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,8 +66,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("nhofmann3");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("Diya Banerjee");
+		  toPrint += p3.toString("dbanerjee30");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
