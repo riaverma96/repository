a126b3e38144bc6a7c3ed78aa918e30faaeee801
dbanerjee30 <dbrbbff7814@gmail.com>
2015-09-01 14:56:42 -0400

    Changed Person 3 in ant lab main

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,7 +67,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Diya Banerjee");
-		  toPrint += p3.toString("dbanerjee30");
+		  toPrint += p3.toString("dbanerjee30.");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
