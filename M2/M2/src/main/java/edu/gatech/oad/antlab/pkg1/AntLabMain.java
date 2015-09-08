4037e7b973d71fabacb0f7b95ada63177ec3acc5
Ria Verma <rverma35@gatech.edu>
2015-09-08 19:22:43 -0400

    created person class files

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -1,11 +1,10 @@
 package edu.gatech.oad.antlab.pkg1;
 
-//
-//import edu.cs2335.antlab.pkg3.*;
-//import edu.gatech.oad.antlab.person.*;
-//import edu.gatech.oad.antlab.pkg2.*;
 
-import edu.gatech.oad.antlab.*;
+import edu.gatech.oad.antlab.person.*;
+import edu.gatech.oad.antlab.pkg2.*;
+
+//import edu.gatech.oad.antlab.*;
 
 /**
  * CS2335 Ant Lab
